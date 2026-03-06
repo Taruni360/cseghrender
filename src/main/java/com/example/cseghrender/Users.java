63dd08ad474c958513774a255b4ab4aaccb5dd9e
Taruni360 <rachakatlasritaruni@gmail.com>
2026-03-06 09:41:26 +0530

    Initial commit

---
+++ src/main/java/com/example/cseghrender/Users.java
@@ -0,0 +1,10 @@
+package com.example.cseghrender;
+
+public class Users {
+
+    public String getEmail() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEmail'");
+    }
+    
+}
